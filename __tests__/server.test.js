17090414836b8ab1d086fc4b912306f529108fca
Franco Sanchez <francodamiansanchez10@gmail.com>
2025-06-12 17:20:45 -0300

    succes test [TRELLO-qkguVDFI]

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('Test de endpoint GET /', () => {
-  it('debería responder con "¡Hola, mundo de CI/CD!"', async () => {
+  it('debería responder con "¡Hola, mundo!"', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('¡Hola, mundo de CI/CD!');
+    expect(res.text).toBe('¡Hola, mundo!');
   });
 });
 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo de CI/CD!');
+  res.send('¡Hola, mundo!');
 });
 
 app.get('/saludo/:nombre', (req, res) => {
